8b62a4fd251ffaff273d1aee2230d25f7d2d67db
ppriewe88 <ppriewe@gmx.net>
2025-05-24 20:25:02 +0200

    fixing wrong name in start skript (api_server -> localrag_api)

--- docker/run_app_docker.sh
+++ docker/run_app_docker.sh
@@ -4,4 +4,4 @@
 # navigate to src directory
 cd /app/src
 # start uvicorn server (api) in docker (& tell it to run in the background)
-uvicorn api_server:app --host 0.0.0.0 --port 8000
+uvicorn localrag_api:app --host 0.0.0.0 --port 8000
